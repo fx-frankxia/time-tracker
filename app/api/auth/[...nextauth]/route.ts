0166d54b991df5d45e73383b2e95b653b153a6b8
FRANK XIA <FX.FRANKXIA@GMAIL.COM>
2023-08-19 11:36:43 +0800

    feat: Next Auth protected pages and users

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
+import { prisma } from "@/lib/prisma";
 
 const authOption: NextAuthOptions = {
   session: {
@@ -8,10 +9,34 @@ const authOption: NextAuthOptions = {
   },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || ' ',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ' ',
-    })
+      clientId: process.env.GOOGLE_CLIENT_ID || " ",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || " ",
+    }),
   ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!profile?.email) {
+        throw new Error("No profile");
+      }
+
+      const user = await prisma.user.upsert({
+        where: {
+          email: profile.email,
+        },
+        create: {
+          email: profile.email,
+          name: profile.name,
+          avatar: (profile as any).picture,
+          tenant: {},
+        },
+        update: {
+          name: profile.name,
+          avatar: (profile as any).picture,
+        },
+      });
+      return true;
+    },
+  },
 };
 
 const handler = NextAuth(authOption);
